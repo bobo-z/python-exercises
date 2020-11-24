2ab6db7d952cc87f69fa39a51b5aabde57d5045c
zyb <804422812@qq.com>
2020-11-24 21:24:35 +0800

    add README

---
+++ exercises/README.md
@@ -0,0 +1,13 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: ybzhang
+ * @Date: 2020-11-24 21:20:50
+ * @LastEditors: ybzhang
+ * @LastEditTime: 2020-11-24 21:24:13
+-->
+# Python Exercise解题过程
+Forked from[Exercism exercises in Python][1]
+
+
+[1]:https://github.com/exercism/python
--- exercises/accumulate/accumulate.py
+++ exercises/accumulate/accumulate.py
@@ -1,2 +1,13 @@
+'''
+Descripttion: 
+version: 
+Author: ybzhang
+Date: 2020-11-24 20:17:35
+LastEditors: ybzhang
+LastEditTime: 2020-11-24 20:59:21
+'''
 def accumulate(collection, operation):
-    pass
+    res=[]
+    for x in collection:
+        res.append(operation(x))
+    return res
